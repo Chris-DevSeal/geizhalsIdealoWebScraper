8d31edbb07897d34a8a04eecc85881f5d22a39a6
Christian Hajek <chris@devseal.eu>
2023-05-16 17:48:27 +0200

    extracted get smallest shipping cost to new method

--- src/main/java/eu/devseal/geizhalsscraper/service/GeizhalsProductService.java
+++ src/main/java/eu/devseal/geizhalsscraper/service/GeizhalsProductService.java
@@ -3,13 +3,26 @@ package eu.devseal.geizhalsscraper.service;
 import eu.devseal.geizhalsscraper.data.GeizhalsProduct;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+
 @Service
 public class GeizhalsProductService {
 
     public double getTotalPrice(GeizhalsProduct product) {
-        double smallestShippingCost = product.getShippingCost().stream()
-                .min(Double::compareTo)
-                .orElse(product.getShippingCost().get(0));
-        return product.getUnitPrice() + smallestShippingCost;
+        return product.getUnitPrice() + getSmallestShippingCost(product.getShippingCost());
+    }
+
+    public String formatDoubleToString(double price) {
+        return String.format("%.2f", price).replace(",", ".");
+    }
+
+    public Double getSmallestShippingCost(List<Double> shippingCost) {
+        if (shippingCost == null) {
+            return 0.0;
+        }
+        return shippingCost.stream()
+                .min(Comparator.naturalOrder())
+                .orElse(shippingCost.get(0));
     }
 }
