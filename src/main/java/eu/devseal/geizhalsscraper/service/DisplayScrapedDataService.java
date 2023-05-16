60d4137bb94ffa46fc89f2f5d20bffb6ad45b33a
Christian Hajek <chris@devseal.eu>
2023-05-16 15:00:36 +0200

    renamed printToConsole() method to displayData() method & extracted some code

--- src/main/java/eu/devseal/geizhalsscraper/service/DisplayScrapedDataService.java
+++ src/main/java/eu/devseal/geizhalsscraper/service/DisplayScrapedDataService.java
@@ -14,13 +14,43 @@ public class DisplayScrapedDataService {
     private final GeizhalsScraperService scraperService;
     private final GeizhalsProductService productService;
 
-    public void printToConsole(Map<Product, List<GeizhalsProduct>> data) {
-        for (Map.Entry<Product, List<GeizhalsProduct>> entry : data.entrySet()) {
-            List<GeizhalsProduct> bestPerformingCompanies = scraperService.findBestPerformingCompanies(entry.getValue());
-            System.out.printf("%s:%n", formatProduct(entry.getKey()));
-            System.out.printf("     Best Performing company: %s - Total Price: %,.2f %n", bestPerformingCompanies.get(0).getCompany(), productService.getTotalPrice(bestPerformingCompanies.get(0)));
-            System.out.printf("     Second best Performing company: %s - Total Price: %,.2f %n", bestPerformingCompanies.get(1).getCompany(), productService.getTotalPrice(bestPerformingCompanies.get(1)));
+    public void displayData(Map<Product, List<GeizhalsProduct>> data) {
+        final String companyName = "Comat";
+        for (Map.Entry<Product, List<GeizhalsProduct>> productEntry : data.entrySet()) {
+            GeizhalsProduct comatListing = scraperService.findProductListingByCompanyName(productEntry.getValue(), companyName)
+                    .orElse(productNotListed());
+            GeizhalsProduct afterComatListing = scraperService.findFirstProductWhereCompanyIsNot(productEntry.getValue(), companyName)
+                    .orElse(productNotListed());
+            printToConsole(productEntry, comatListing, afterComatListing);
         }
+    }
+
+    private void printToConsole(Map.Entry<Product, List<GeizhalsProduct>> entry, GeizhalsProduct comatListing, GeizhalsProduct afterComatListing) {
+        System.out.printf("%s:%n", formatProduct(entry.getKey()));
+        displayComatListing(comatListing);
+        displayCompetitorListing(afterComatListing);
+    }
+
+    private void displayCompetitorListing(GeizhalsProduct afterComatListing) {
+        if (afterComatListing.getOfferID() == 999) {
+            System.out.println("     Best not Comat Listing: No other listing");
+        } else {
+            System.out.printf("     Best not Comat Listing: %s - Total Price: %,.2f %n", afterComatListing, productService.getTotalPrice(afterComatListing));
+        }
+    }
+
+    private void displayComatListing(GeizhalsProduct comatListing) {
+        if (comatListing.getOfferID() == 999) {
+            System.out.println("     Comat Listing: No Comat listing");
+        } else {
+            System.out.printf("     Comat Listing: %s - Total Price: %,.2f %n", comatListing, productService.getTotalPrice(comatListing));
+        }
+    }
+
+    private GeizhalsProduct productNotListed() {
+        return GeizhalsProduct.builder()
+                .offerID(999)
+                .build();
     }
 
     private String formatProduct(Product product) {
@@ -28,6 +58,6 @@ public class DisplayScrapedDataService {
         for (int i = 0; i < prodArr.length; i++) {
             prodArr[i] = prodArr[i].substring(0, 1).toUpperCase() + prodArr[i].substring(1);
         }
-        return String.format("Saeco %s",String.join(" ", prodArr));
+        return String.format("Saeco %s", String.join(" ", prodArr));
     }
 }
