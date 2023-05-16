68cbc728f64d88d748b71e16a0ebc7f5f56cffa4
Christian Hajek <chris@devseal.eu>
2023-05-16 17:43:48 +0200

    Added CSV Writer and Filewriter Bean

--- src/main/java/eu/devseal/geizhalsscraper/runner/ScraperRunner.java
+++ src/main/java/eu/devseal/geizhalsscraper/runner/ScraperRunner.java
@@ -1,5 +1,6 @@
 package eu.devseal.geizhalsscraper.runner;
 
+import eu.devseal.geizhalsscraper.IO.CSVWriterService;
 import eu.devseal.geizhalsscraper.data.GeizhalsProduct;
 import eu.devseal.geizhalsscraper.data.Product;
 import eu.devseal.geizhalsscraper.service.DisplayScrapedDataService;
@@ -7,7 +8,11 @@ import eu.devseal.geizhalsscraper.service.GeizhalsScraperService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +24,19 @@ public class ScraperRunner {
             Map<Product, List<GeizhalsProduct>> products = scraperService.scrape();
             display.displayData(products);
         };
+    }
+
+    @Bean
+    @Order(1)
+    ApplicationRunner writeToCSV(CSVWriterService writerService, FileWriter fileWriter) {
+        return args -> {
+            writerService.writeScrapedDataToCsv(fileWriter);
+        };
+    }
+    @Bean
+    FileWriter getFileWriter() throws IOException {
+        File file = new File("src/main/resources/output/tmp.csv");
+        return new FileWriter(file);
     }
 
 }
