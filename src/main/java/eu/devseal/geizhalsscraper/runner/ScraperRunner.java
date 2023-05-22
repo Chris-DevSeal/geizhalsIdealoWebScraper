a07fe7a5a280808aee4139e7ff348c72be558333
Christian Hajek <chris@devseal.eu>
2023-05-22 12:40:36 +0200

    Removed ConsoleWriter on App Start

--- src/main/java/eu/devseal/geizhalsscraper/runner/ScraperRunner.java
+++ src/main/java/eu/devseal/geizhalsscraper/runner/ScraperRunner.java
@@ -3,12 +3,10 @@ package eu.devseal.geizhalsscraper.runner;
 import eu.devseal.geizhalsscraper.IO.CSVWriterService;
 import eu.devseal.geizhalsscraper.data.GeizhalsProduct;
 import eu.devseal.geizhalsscraper.data.Product;
-import eu.devseal.geizhalsscraper.service.DisplayScrapedDataService;
 import eu.devseal.geizhalsscraper.service.GeizhalsScraperService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,16 +16,8 @@ import java.util.Map;
 
 @Configuration
 public class ScraperRunner {
-    @Bean
-    ApplicationRunner runScraper(GeizhalsScraperService scraperService, DisplayScrapedDataService display) {
-        return args -> {
-            Map<Product, List<GeizhalsProduct>> products = scraperService.scrape();
-            display.displayData(products);
-        };
-    }
 
     @Bean
-    @Order(1)
     ApplicationRunner writeToCSV(GeizhalsScraperService scraperService, CSVWriterService writerService, FileWriter fileWriter) {
         return args -> {
             Map<Product, List<GeizhalsProduct>> data = scraperService.scrape();
