c50519c234765af3f002dc27c96807543997122c
Christian Hajek <chris@devseal.eu>
2023-05-22 12:15:34 +0200

    extracted 999_999_999 to static constant

--- src/main/java/eu/devseal/geizhalsscraper/data/StaticConstants.java
+++ src/main/java/eu/devseal/geizhalsscraper/data/StaticConstants.java
@@ -3,6 +3,7 @@ package eu.devseal.geizhalsscraper.data;
 
 public class StaticConstants {
     public static final int NO_LISTING_ID = 999;
+    public static final int NO_LISTING_VALUE = 999_999_999;
     public static final String COMPANY = "Comat";
 
 }
