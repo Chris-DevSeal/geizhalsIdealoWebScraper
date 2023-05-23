b6f74e99dfeb0fef9976afc06a9ff10719b5b806
Christian Hajek <chris@devseal.eu>
2023-05-23 11:27:53 +0200

    Refactored Runner to AppConfiguration

--- src/main/java/eu/devseal/geizhalsscraper/configuration/AppConfigruation.java
+++ src/main/java/eu/devseal/geizhalsscraper/configuration/AppConfigruation.java
@@ -1,4 +1,4 @@
-package eu.devseal.geizhalsscraper.runner;
+package eu.devseal.geizhalsscraper.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import java.io.File;
 
 @Configuration
-public class ScraperRunner {
+public class AppConfigruation {
 
     @Bean
     File file() {
