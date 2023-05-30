ff55066490fcb3b444f8c2f7cfd8cf421e16b8ae
Christian Hajek <chris@devseal.eu>
2023-05-30 12:50:57 +0200

    Renamed to GeizhalsCssQuery & changed attr. to url

--- src/main/java/eu/devseal/geizhalsscraper/data/GeizhalsCssQuery.java
+++ src/main/java/eu/devseal/geizhalsscraper/data/GeizhalsCssQuery.java
@@ -1,14 +1,14 @@
 package eu.devseal.geizhalsscraper.data;
 
-public enum CssQuery {
+public enum GeizhalsCssQuery {
     PRODUCT_LISTINGS("div.row.offer"),
     SHIPPING_COST("div.offer__delivery div.offer__delivery-payment span.gh_extracost"),
     COMPANY("div.offer__merchant a"),
     COMPANY_ATTRIBUTE("data-merchant-name"),
     UNIT_PRICE("div.offer__price span");
-    public final String geizhals;
+    public final String url;
 
-    CssQuery(String geizhals) {
-        this.geizhals = geizhals;
+    GeizhalsCssQuery(String url) {
+        this.url = url;
     }
     }
