55e451611f3f5f8a40fc45a1d50c227db37a8594
Christian Hajek <chris@devseal.eu>
2023-05-30 13:21:45 +0200

    Added Idealo to refreshScrapeResults & refactored Controller logic to this class

--- src/main/java/eu/devseal/geizhalsscraper/service/GeizhalsWebService.java
+++ src/main/java/eu/devseal/geizhalsscraper/service/GeizhalsWebService.java
@@ -1,14 +1,17 @@
 package eu.devseal.geizhalsscraper.service;
 
 import eu.devseal.geizhalsscraper.IO.CSVWriterService;
-import eu.devseal.geizhalsscraper.data.GeizhalsProduct;
 import eu.devseal.geizhalsscraper.data.Product;
+import eu.devseal.geizhalsscraper.data.ProductListing;
+import eu.devseal.geizhalsscraper.exceptions.CustomFileNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
 
@@ -16,16 +19,37 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
 public class GeizhalsWebService {
-    private final GeizhalsScraperService scraperService;
+    private final GeizhalsScraperService geizhalsScraperService;
+    private final IdealoScraperService idealoScraperService;
     private final CSVWriterService writerService;
 
     public void refreshScrapeResults(FileWriter fileWriter) {
         try {
-            Map<Product, List<GeizhalsProduct>> data = scraperService.scrape();
-            writerService.writeScrapedDataToCsv(data, fileWriter);
+            Map<Product, List<ProductListing>> scrapedData = geizhalsScraperService.scrape();
+            Map<Product, List<ProductListing>> secondProviderScrapedData = idealoScraperService.scrape();
+            for (Map.Entry<Product, List<ProductListing>> productListEntry : scrapedData.entrySet()) {
+                productListEntry.getValue().addAll(secondProviderScrapedData.get(productListEntry.getKey()));
+            }
+            writerService.writeScrapedDataToCsv(scrapedData, fileWriter);
         } catch (IOException e) {
             log.error("Error: " + e);
         }
     }
 
+    public byte[] getCsvData(boolean reload, File file) throws CustomFileNotFoundException {
+        if (reload || !file.exists()) {
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                refreshScrapeResults(fileWriter);
+            } catch (IOException e) {
+                log.error("Controller Error: "+e);
+            }
+        }
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            log.error("Not able to read / create CSV file");
+            throw new CustomFileNotFoundException();
+        }
+
+    }
 }
