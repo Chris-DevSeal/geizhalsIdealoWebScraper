fbff79de6ab2dcb5114204497ac1a1135df0c058
Christian Hajek <chris@devseal.eu>
2023-05-31 12:46:25 +0200

    Removed unused Import

--- src/main/java/eu/devseal/geizhalsscraper/controller/GeizhalsController.java
+++ src/main/java/eu/devseal/geizhalsscraper/controller/GeizhalsController.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-
 @RestController
 @RequestMapping("api/v1/geizhals")
 @RequiredArgsConstructor
