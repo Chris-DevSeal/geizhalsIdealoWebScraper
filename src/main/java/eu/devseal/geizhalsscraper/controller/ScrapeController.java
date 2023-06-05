585c895fb48e3958b56ca5193a20f08288d4ec34
Christian Hajek <chris@devseal.eu>
2023-06-05 16:01:26 +0200

    Renamed GeizhalsController

--- src/main/java/eu/devseal/geizhalsscraper/controller/ScrapeController.java
+++ src/main/java/eu/devseal/geizhalsscraper/controller/ScrapeController.java
@@ -1,7 +1,7 @@
 package eu.devseal.geizhalsscraper.controller;
 
 import eu.devseal.geizhalsscraper.exceptions.CustomFileNotFoundException;
-import eu.devseal.geizhalsscraper.service.GeizhalsWebService;
+import eu.devseal.geizhalsscraper.service.ScrapeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/geizhals")
 @RequiredArgsConstructor
 @Slf4j
-public class GeizhalsController {
-    private final GeizhalsWebService geizhalsWebService;
+public class ScrapeController {
+    private final ScrapeService scrapeService;
 
     @GetMapping(produces = "text/csv")
     @CrossOrigin
     byte[] evaluateScrapedData(@RequestParam(defaultValue = "false") boolean reload) throws CustomFileNotFoundException {
-        return geizhalsWebService.getCsvData(reload);
+        return scrapeService.getCsvData(reload);
     }
 
 }
